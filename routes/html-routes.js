6efbeea4ed9a32251f0a3421a2863f5a5ec2e644
loc-koan <loc.koan@yahoo.com>
2020-07-26 15:12:54 -0500

    var on hbobject

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,5 @@
 // Requiring our custom middleware for checking if a user is logged in
-const isAuthenticated = require('../config/middleware/isAuthenticated');
+// const isAuthenticated = require('../config/middleware/isAuthenticated');
 
 module.exports = function (app) {
     app.get('/', (req, res) => {
