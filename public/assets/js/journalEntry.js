7db1efab5610fbdbc541bb4a3bffabeb03c737c8
On-Your-Marc-GO <marcos.a.arguijo@gmail.com>
2020-07-29 00:10:31 -0500

    activity Modal Entry logic?

--- public/assets/js/journalEntry.js
+++ public/assets/js/journalEntry.js
@@ -1,44 +1,40 @@
 /*eslint arrow-body-style: ["error", "as-needed"]*/
 /*eslint-env es6*/
 $(document).ready(() => {
-    // Getting references to our form and inputs
-    const journalEntryForm = $('form.login');
-    const emailInput = $('input#email-input');
-    const passwordInput = $('input#password-input');
+    const modalActivityTitle = $('input#ActivityTitle');
+    const modalActivityCategory = $('select##CategorySelect');
+    const modalActivityTime = $('input#ActivityTime');
+    const modalActivityNotes = $('textarea#ActivityNotes');
 
-    // When the form is submitted, we validate there's an email and password entered
-    journalEntryForm.on('submit', (event) => {
-        const userData = {
-            email: emailInput.val().trim(),
-            password: passwordInput.val().trim(),
+    actvityEntryForm.on('submit', (event) => {
+        const activityData = {
+            activityName: modalActivityTitle.val().trim(),
+            activityCategory: modalActivityCategory.val().trim(),
+            totalTime: modalActivityTime.val().trim(),
+            entryActvityText: modalActivityNotes.val().trim(),
         };
+
         event.preventDefault();
 
-        if (!userData.email || !userData.password) {
-            return;
-        }
-
-        // If we have an email and password we run the loginUser function and clear the form
-        loginUser(userData.email, userData.password);
-        emailInput.val('');
-        passwordInput.val('');
+        addActivity(
+            activityData.activityCategory,
+            activityData.activityCategory,
+            activityData.totalTime,
+            activityData.entryActvityText
+        );
     });
 
-    // loginUser does a post to our "api/login" route and if successful, redirects us the the index page
-    function loginUser(email, password) {
-        $.post('/login', {
-            email: email,
-            password: password,
-        })
-            .then(() => {
-                window.location.replace('/dashboard');
-                // If there's an error, log the error
-            })
-            .catch(handleLoginErr);
-    }
-
-    function handleLoginErr(err) {
-        $('#alert .msg').text(JSON.stringify(err.responseJSON));
-        $('#alert').fadeIn(500);
+    function addActivity(
+        activityName,
+        activityCategory,
+        totalTime,
+        entryActvityText
+    ) {
+        $.post('api/activityEntry', {
+            activityName: activityName,
+            activityCategory: activityCategory,
+            totalTime: totalTime,
+            entryActvityText: entryActvityText,
+        });
     }
 });
